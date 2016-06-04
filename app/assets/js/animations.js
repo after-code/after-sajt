2a7d1b420c680af66186e0b0e51bcda6eaf97e25
Vasilije Milenkovic <vasilie@Vasilijes-MacBook-Pro.local>
2016-06-04 21:32:20 +0200

    designin part2

--- app/assets/js/animations.js
+++ app/assets/js/animations.js
@@ -1,6 +1,17 @@
 function showActions(){
   setTimeout(function(){
-    $(".fold-actions").animate({"bottom":"0px"},300);
-    $(".main-heading").animate({"opacity":"1"},300);
+    $(".fold-actions").animate({"bottom":"0px"},900, 'easeInOutExpo');
+    $(".main-heading").animate({"opacity":"1"},900, 'easeInOutExpo');
   },400);
 }
+var first = true;
+function changeScene(){
+  $("body").animate({scrollTop: $(".viewport").height()},1500, 'easeInOutExpo');
+  $("body").css({"overflow-y":"scroll"});
+}
+$("body, html").scroll(function(){
+  if (!first){
+    changeScene();
+    first = false;
+  }
+});

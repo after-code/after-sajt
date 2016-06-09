a989146dd2563491841343e880913e6f809e15ce
Vasilije Milenkovic <vasilie@Vasilijes-MacBook-Pro.local>
2016-06-09 20:58:51 +0200

    scroll js

--- app/assets/js/animations.js
+++ app/assets/js/animations.js
@@ -1,7 +1,7 @@
 var foldShown = false;
 function showActions(){
   setTimeout(function(){
-    $(".fold-actions").animate({"bottom":"0px"},900, 'easeInOutExpo', function(){
+    $(".fold-actions").animate({"top":"-100%"},900, 'easeInOutExpo', function(){
       foldShown = true;
     });
     $(".main-heading").animate({"opacity":"1"},900, 'easeInOutExpo');
@@ -10,15 +10,17 @@ function showActions(){
 var first = true;
 
 function changeScene(){
-  $("body").animate({scrollTop: $(".viewport").height()},1200, 'easeInOutCubic', function(){
+  $("body").animate({scrollTop: $(".viewport").height()},600, 'easeInOutCubic', function(){
     $("body").css({"overflow-y":"scroll"});
+    first = false;
     console.log('finished');
     if (foldShown){
-      $(".fold-actions").css({"display":"none"});
+      // $(".fold-actions").css({"display":"none"});
     }
   });
+  $(".fold-actions").animate({"opacity":"0"}, 300);
   if (!foldShown){
-    $(".fold-actions").css({"display":"none"});
+    // $(".fold-actions").css({"display":"none"});
   }
 }
 $(document).bind('mousewheel', function(evt) {
@@ -28,13 +30,6 @@ $(document).bind('mousewheel', function(evt) {
       console.log('scrolling');
     }
 });
-$(function(){
-  // $("body, html").scrollTop(0);
-  $(window).scrollTop(0);
-  $("html").scrollTop(0);
-  console.log("asfs");
-
+$(window).on('unload', function() {
+    $(window).scrollTop(0);
 });
-setTimeout(function(){
-   $(window).scrollTop(0);
-},200);

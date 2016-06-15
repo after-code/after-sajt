bfc2c4497aa38a5cf1a3f4b7b6dbb7cf8b5b3cfb
Bogdan <bogdanbrkic@gmail.com>
2016-06-15 15:35:36 +0200

    on win resize..

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -503,3 +503,15 @@ tween12.onUpdate(function(){
      requestAnimationFrame(render);
     }
     console.log("bg");
+
+/**/
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	composer.setSize( window.innerWidth, window.innerHeight );
+}
+
+//
+window.addEventListener( 'resize', onWindowResize, false );
